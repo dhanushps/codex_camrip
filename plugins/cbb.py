d5e2eacbcd4a4c4a875627084d436cbe1424b11f
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-05-08 15:30:25 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='https://tg://settings'>Is It U ❓😂</a>\n○ Channel : @Camprint_movies</b>",
+            text = f"<b>○ Creator : <a href='tg://settings'>Is It U ❓😂</a>\n○ Channel : @Camprint_movies</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
